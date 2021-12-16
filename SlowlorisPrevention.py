ecba87e388e458b1d21bdc31c08c7fbdd7e7ac2d
Goldy Gour <64465355+21goldy@users.noreply.github.com>
2021-12-17 00:49:52 +0530

    Update SlowlorisPrevention.py

--- SlowlorisPrevention.py
+++ SlowlorisPrevention.py
@@ -2,43 +2,42 @@ import os
 import subprocess
 import time
 
-connectionTotal = 15
+allowedConnections = 15
 refreshRate = 3
 
-
-blockedips = []
-connums = []
+blocked_ips = []
+blank_list = []
 ips = []
 
 while True:
     if os.geteuid() != 0:
-        print("This script requires root privileges. Exiting.")
+        print("Authentication Required!\n No root privileges.")
         break
     if os.path.isdir('/etc/ufw/'):
-        ufws = subprocess.Popen(["ufw","status"], stdout=subprocess.PIPE)
-        s = str(ufws.communicate())
-        if 'inactive' in s:
+        Uncomplicated_firewall = subprocess.Popen(["ufw", "status"], stdout=subprocess.PIPE)
+        response = str(Uncomplicated_firewall.communicate())
+        if 'inactive' in response:
             print('UFW Disabled. To enable, enter `sudo ufw enable` into your terminal.')
             break
     else:
         print('UFW not installed. To install, enter `sudo apt-get install ufw` into your terminal.')
-        break            
-    f = open('blockedIPs.txt','a')
-    ns = os.popen("netstat -ntu|awk '{print $5}'|cut -d: -f1 -s|sort|uniq -c|sort -nk1 -r")
-    ipl = ns.read()
-    l = list(ipl.split())
-    for x in range(len(l)):
+        break
+    file = open('blockedIPs.txt', 'a')
+    connectionDetails = os.popen("netstat -ntu|awk '{print $5}'|cut -d: -f1 -s|sort|uniq -c|sort -nk1 -r")
+    readDetails = connectionDetails.read()
+    scrapedIPs = list(readDetails.split())
+    for x in range(len(scrapedIPs)):
         if x % 2 == 0:
-            connums.append(l[x])
+            blank_list.append(scrapedIPs[x])
         else:
-            ips.append(l[x])
-    for x, y in enumerate(connums):
-        if int(y) > connectionTotal:
-            if ips[x] != '127.0.0.1' and ips[x] not in blockedips:
+            ips.append(scrapedIPs[x])
+    for x, y in enumerate(blank_list):
+        if int(y) > allowedConnections:
+            if ips[x] != '127.0.0.1' and ips[x] not in blocked_ips:
                 print('Blocking %s with %d connections' % (ips[x], int(y)))
                 os.system(str('ufw insert 2 deny from %s' % ips[x]))
                 os.system(str('ufw reload'))
-                blockedips.append(ips[x])
-                f.write(ips[x] + '\n')   
-    f.close()
+                blocked_ips.append(ips[x])
+                file.write(ips[x] + '\n')
+    file.close()
     time.sleep(refreshRate)
